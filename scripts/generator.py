e5d3e2d65f3802bf7bea01e9f718e016d5c0d27f
stormhammersecuritysacramento <stormhammer.security.sacramento@gmail.com>
2026-06-08 15:44:04 -0700

    Update generator.py

--- scripts/generator.py
+++ scripts/generator.py
@@ -9,20 +9,20 @@ os.makedirs('docs')
 # Read data from the data folder
 df = pd.read_csv('data/pages.csv')
 
-# Load the template - we will look in the 'templates' folder specifically
+# Load the template
 template_path = os.path.join('templates', 'template.html')
 with open(template_path, 'r') as f:
     template = Template(f.read())
 
-# Generate files
+# Generate individual service pages
 for _, row in df.iterrows():
     filename = f"docs/{row['filename']}.html"
     with open(filename, 'w') as f:
         f.write(template.render(row=row))
 
-# Generate simple index.html so you don't get 404s
+# Generate simple index.html for navigation
 with open('docs/index.html', 'w') as f:
-    f.write("<h1>Service Pages</h1><ul>")
+    f.write("<h1>StormHammer Security Service Pages</h1><ul>")
     for _, row in df.iterrows():
         f.write(f"<li><a href='{row['filename']}.html'>{row['title']}</a></li>")
     f.write("</ul>")
